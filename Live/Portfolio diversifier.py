d830e2f3238e623f8fb21e891541b9e7c9bf1de1
caocchinh <chinhcaocu@gmail.com>
2026-02-01 11:45:43 +0700

    feat: Update portfolio asset allocations, refine TradeExecutor initialization to use the full portfolio, and enable a test buy operation with a specified amount.

--- Live/Portfolio diversifier.py
+++ Live/Portfolio diversifier.py
@@ -6,26 +6,25 @@ from TradeExecutor import TradeExecutor
 def main():
     capital_asset = "USDT"
     portfolio = {
-        "L_VTHO_1.5X": 4.35,
-        "L_SKL_1X": 4.35,
-        "L_SEI_1X": 4.35,
-        'S_DOGE_3X': 2.18,
-        "L_AVAX_1X": 4.35,
-        "L_GLMR_1X": 48,
-        'S_IMX_2X': 3.75,
-        'S_INJ_2X': 3.75,
+        "L_VTHO_1.5X": 2.83,
+        "L_SKL_1X": 2.83,
+        "L_SEI_1X": 2.83,
+        'S_DOGE_3X': 1.42,
+        "L_AVAX_1X": 2.83,
+        "L_GLMR_1X": 31.14,
+        'S_IMX_2X': 2.44,
+        'S_INJ_2X': 2.44,
         "L_COTI_1X": 0,
-        'S_BEAMX_9X': 2.06,
+        'S_BEAMX_9X': 1.34,
         "L_ARB_1X": 0,
-        "L_BTC_1X":10,
-        'S_MINA_9X': 2.06,
-        'S_FTM_3X': 2.99,
-        'L_IMX_3X': 4.75,
-        "L_FET_10X":30,
-        "L_FIL_10X": 30,
+        "L_BTC_1X": 6.50,
+        'S_MINA_9X': 1.34,
+        'L_IMX_3X': 3.08,
+        "L_FET_10X": 19.49,
+        "L_FIL_10X": 19.49,
     }
 
-    static_order = TradeExecutor(capital_asset, list(portfolio.keys()), 100, margin_type="cross",
+    static_order = TradeExecutor(capital_asset, portfolio, 100, margin_type="cross",
                                  margin=True, confirmation=True
                                  , vip=0, global_leverage=2,
                                  global_order_side="long", delay=5)
@@ -35,8 +34,8 @@ def main():
     # static_order.sell(everything=True, convert=False, amount="100%")
     # static_order.repay(everything=True)
     # time.sleep(5)
-    static_order.internal_transfer(specific_symbols=["USDT"], from_place="cross", to_place="spot", isolated_transfer_asset="base")
-    # static_order.buy()
+    # static_order.internal_transfer(specific_symbols=["USDT"], from_place="cross", to_place="spot", isolated_transfer_asset="base")
+    static_order.buy(test_money=1000)
 
 
 if __name__ == "__main__":
